e1a040e5d09100366eb92655c706d0afc695584d
atif <ratul0947@gmail.com>
2022-12-25 23:19:09 +0600

    basic graphql api

--- server/server.js
+++ server/server.js
@@ -2,11 +2,43 @@ const express = require('express')
 const app = express()
 const port =process.env.PORT || 8000
 const bodyParser = require('body-parser')
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql')
+
 
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/graphql', graphqlHTTP({
+    schema : buildSchema(`
+
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation:RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+        }
+        ,
+        createEvent: (args) => {
+            const eventName = args.name;
+            return "hello " + eventName;
+        }
+       
+    },
+    graphiql: true
+}))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-//dd
